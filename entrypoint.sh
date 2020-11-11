92680609d10be3fbf6c1e2dfbe6a1d72c1558c20
Matthew Lutze <mcdlutze@gmail.com>
2020-11-11 13:34:29 +0100

    run flix tests

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,21 @@
 #!/bin/sh -l
 
-echo "Hello $1"
-time=$(date)
-echo "::set-output name=time::$time"
+flix_version="$1"
+test_dir="$2"
+fail_symbol="✗"
+
+# make a temporary dir
+mkdir tmp
+
+# get the flix jar
+curl -L "https://github.com/flix/flix/releases/download/$flix_version/flix.jar" --output flix.jar
+
+# run tests and store the output
+java -jar flix.jar --test "$test_dir"/**.flix | tee tmp/test-output
+
+# check the output for errors
+if grep fail_symbol tmp/test-output; then
+    exit 1
+else
+    exit 0
+fi
